7acb9013e06bf564854f10954da77f9ce3f8075b
smallmentor <s92203aaa@gmail.com>
2018-03-03 03:06:14 +0800

    單向繫結:個人資料

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  school:string = '南台科技大學'
+  name:string = '鍾小師'
+  textColor:string = '#333'
+  imgURL:string = 'mugShot.jpg'
   constructor(public navCtrl: NavController) {
-
+    
   }
 
 }
